4d26cafd1f19f496c23eae7dd4093b3db134a00b
Janice Teguh <janicejarusdy@gmail.com>
2022-09-23 18:02:10 +1000

    inserted animation for timeline

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const downArrow = document.getElementById("arrow");
 const main = document.getElementsByTagName("main")[0];
-downArrow.addEventListener('click', function() {
+downArrow.addEventListener('click', function () {
     main.scrollIntoView();
+})
+
+const events = document.querySelectorAll(".timeline__event")
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+
+}, { threshold: 0.6})
+
+events.forEach(event => {
+    observer.observe(event)
 })
